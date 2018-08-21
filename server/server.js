07bd9d4eea11fd317df93fc4c1c46069d8364cbc
Shadi <floydanthebeast@gmail.com>
2018-08-21 22:06:49 +0300

    API improvements: add methods for books and persons

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ app.get('/lessons/:section/:name', (req, res) => {
 
 app.get('/lessonsList', (req, res) => {
     let filePaths = glob.sync(path.join(__dirname, '/data/lessons/*/*.md'));
+
     if (filePaths) {
         let fileStructure = {};
         
@@ -50,8 +51,53 @@ app.get('/lessonsList', (req, res) => {
     }
 });
 
+app.get('/persons/:name', (req, res) => {
+    const basePath = path.join(__dirname, `/data/persons/${req.params.name}/`);
+    const personsInfo = JSON.parse(fs.readFileSync(path.join(basePath, 'data.json')));
+    const personsBio = fs.readFileSync(path.join(basePath, 'bio.md'));
+
+    res.send({
+        info: personsInfo,
+        bio: personsBio.toString()
+    });
+});
+
+app.get('/personsList', (req, res) => {
+    const filePaths = glob.sync(path.join(__dirname, '/data/persons/*/*.json'));
+    let listOfPersons = {};
+
+    for (let filePath of filePaths) {
+        const personsData = JSON.parse(fs.readFileSync(filePath));
+        const pathDirs = path.dirname(filePath).split(path.sep);
+        const personsName = pathDirs[pathDirs.length - 1];
+
+        listOfPersons[personsName] = personsData;
+    }
+    
+    res.send(listOfPersons);
+});
+
+app.get('/books/:name', (req, res) => {
+    const file = fs.readFileSync(path.join(__dirname, `data/books/${req.params.name}.json`));
+    res.send(file);
+});
+
+app.get('/booksList', (req, res) => {
+    const filePaths = glob.sync(path.join(__dirname, 'data/books/*.json'));
+    console.log(filePaths)
+    let listOfBooks = []
+
+    for (filePath of filePaths) {
+        const bookName = path.basename(filePath);
+
+        listOfBooks.push(bookName);
+    }
+
+    res.send(listOfBooks);
+});
+
 const sortLessons = (fileStructure, config) => {
-    Array.prototype.contains = function(element){
+    Array.prototype.contains = element => {
         return this.indexOf(element) > -1;
     };
 
